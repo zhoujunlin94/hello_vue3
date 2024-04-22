d888d6a624ade25b47a0ca5055cdc48c1887cf98
zhoujunlin <zhoujunlin.work@outlook.com>
2024-04-22 22:40:45 +0800

    组件注册

---
+++ backup/src03/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="updateName">修改名字</button>
+        <button @click="updateAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Person', // 组件名
+    data() {
+        return {
+            name: '张三',
+            age: 30,
+            tel: 18500000000
+        }
+    }, // 数据
+    methods: {
+        updateName() {
+            this.name = 'zhang-san'
+        },
+        updateAge() {
+            this.age += 1
+        },
+        showTel() {
+            alert(this.tel)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+</style>
