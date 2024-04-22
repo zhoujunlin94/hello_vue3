a8029628872f490d45d0d8d706ad8e7454072fbe
zhoujunlin <zhoujunlin.work@outlook.com>
2024-04-22 22:02:12 +0800

    bakup

---
+++ backup/src02/App.vue
@@ -0,0 +1,24 @@
+<!-- html -->
+<template>
+    <div class="app">
+        <h1>你好啊</h1>
+    </div>
+</template>
+
+
+<!-- JS/TS -->
+<script lang="ts">
+    export default { // 暴露组件
+        name: 'App' // 组件名
+    }
+</script>
+
+<!-- 样式 -->
+<style>
+    .app {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
